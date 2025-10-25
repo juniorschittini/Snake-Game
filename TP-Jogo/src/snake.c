a3a1715b777a027af3cbea7e76541e9b19384aa1
juniorschittini <164209873+juniorschittini@users.noreply.github.com>
2025-10-25 15:01:40 -0300

    Update snake.c

--- TP-Jogo/src/snake.c
+++ TP-Jogo/src/snake.c
@@ -14,9 +14,10 @@
 #define SNAKE_COLOR YELLOW
 #define FOOD_COLOR BLUE
 
-
+int gameOver = 1;
 
 void StartBody(Game *g){
+    //Inicia a posição, direção e cor da cobra
     g->body.pos = (Rectangle) {LARGURA/2 - STD_SIZE_X, ALTURA - STD_SIZE_Y -10, STD_SIZE_X, STD_SIZE_Y};
     g->body.direction = 0;
     g->body.color = SNAKE_COLOR;
@@ -34,11 +35,13 @@ void StartBordas(Game *g){
 }
 
 void StartFood(Game *g){
+    //Gera uma posição aleatória para a comida
     g->food.pos = (Rectangle) {(float)(rand() % ((ALTURA - 20) / STD_SIZE_Y) * STD_SIZE_Y + 10), (float)(rand() % ((ALTURA - 20) / STD_SIZE_Y) * STD_SIZE_Y + 10), STD_SIZE_X, STD_SIZE_Y};
     g->food.color = FOOD_COLOR;
 }
 
 void StartRound(Game *g){
+    //Inicia uma nova rodada
     StartBordas(g);
     StartBody(g);
     StartFood(g);
@@ -49,30 +52,43 @@ void DrawBody(Game *g){
     DrawRectangleRec(g->body.pos, g->body.color);
     if(g->body.pos.x < 10 || g->body.pos.x + STD_SIZE_X > LARGURA - 10 ||
        g->body.pos.y < 10 || g->body.pos.y + STD_SIZE_Y > ALTURA - 10){
-        //Se colidir com a borda, reStart o game
-        StartRound(g);
+        //Se a cobra sair da área de jogo, reinicia o jogo
+        gameOver = 0;
+        DrawText("YOU LOOSE!", 105, 230, 80, WHITE);
+        DrawText("PRESS \"ENTER\" TO CONTINUE", 165, 600, 20, WHITE);
+        //Se o jogador apertar enter, reinicia a rodada
+        if (IsKeyPressed(KEY_ENTER)){
+            //atualiza o jogo para reiniciar a rodada
+            gameOver = 1;
+            StartRound(g);
+        }   
     }
 }
 
 void DrawFood(Game *g){
-    DrawRectangleRec(g->food.pos, g->food.color);
+    //Desenha a comida so se o jogo não tiver acabado
+    if(gameOver != 0){
+        DrawRectangleRec(g->food.pos, g->food.color);
+    }
 }
 
+
 void DrawBordas(Game *g){
-    //Draw as barreiras nas bordas
+    //Desenha as barreiras nas bordas
     for (int i = 0; i < 4; i++){
         DrawRectangleRec(g->bordas[i].pos, LIGHTGRAY);
     }
 }
 
 void Drawgame(Game *g){
+    //Desenha o jogo
     DrawBordas(g);
     DrawBody(g);
     DrawFood(g);
 }
 
 void Updatedirection(Game *g){
-    //Update para qual direção a cobra vai  
+    //Atualiza para qual direção a cobra vai  
     if(IsKeyDown(KEY_UP) && g->body.direction != 2 && GetTime() - g->cooldown > COOLDOWN){
         g->body.direction = 0;
         g->cooldown = GetTime();
@@ -91,8 +107,10 @@ void Updatedirection(Game *g){
     }
 }
 
-void UpdatePosBody(Game *g){
 
+
+void UpdatePosBody(Game *g){
+    // Atualiza a posição da cobra conforme a direção
     if (g->body.direction == 0){
         g->body.pos.y -= STD_SIZE_Y;
     }
@@ -108,12 +126,14 @@ void UpdatePosBody(Game *g){
 }
 
 void UpdatePosFood(Game *g){
+    //Gera uma nova posição para a comida
     g->food.pos = (Rectangle) {(float)(rand() % ((ALTURA - 20) / STD_SIZE_Y) * STD_SIZE_Y + 10), 
         (float)(rand() % ((ALTURA - 20) / STD_SIZE_Y) * STD_SIZE_Y + 10), STD_SIZE_X, STD_SIZE_Y};
 
 }
 
 void UpdateRodada(Game *g){
+    // Atualiza o estado do jogo a cada frame
     Updatedirection(g);
     if (GetTime() - g->time > TIME){
         UpdatePosBody(g);
@@ -127,6 +147,7 @@ void UpdateRodada(Game *g){
 }
 
 int CollisionFood(Game *g){
+    //Verifica se a cobra colidiu com a comida
     if (CheckCollisionRecs(g->body.pos, g->food.pos)){
         return 1;
     }
