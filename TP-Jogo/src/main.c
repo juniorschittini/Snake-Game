305b870a40147c7b61d8f5c51af9ef16fbc4f650
juniorschittini <164209873+juniorschittini@users.noreply.github.com>
2025-10-25 15:02:20 -0300

    Refactor main game loop and cleanup code
    Removed unnecessary game over handling and text display.

--- TP-Jogo/src/main.c
+++ TP-Jogo/src/main.c
@@ -14,11 +14,12 @@
 #define SNAKE_COLOR YELLOW
 #define FOOD_COLOR BLUE
 
+
+
 int main(){
     Game game;
     int gameOver = 1;
-
-    //Cria a ganela;
+    //Cria a janela;
     InitWindow(LARGURA, ALTURA, "Snake Game");
     SetTargetFPS(60);
     srand(time(NULL));
@@ -33,14 +34,7 @@ int main(){
             if (CollisionFood(&game)){
                 gameOver = 1;
             }
-        } else {
-            DrawText("Agora é a sua vez!", 150, 200, 40, WHITE);
-            DrawText("Continue", 265, 400, 30, WHITE);
-            if (IsKeyPressed(KEY_ENTER)){
-                StartRound(&game);
-                gameOver = 0;
-            }
-        }
+        } 
         EndDrawing();
     }
     CloseWindow();
