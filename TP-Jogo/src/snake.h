5d3230dae7de2d626d7f0c892f6518cbdc9d3259
juniorschittini <164209873+juniorschittini@users.noreply.github.com>
2025-10-26 01:57:56 -0300

    Refactor snake.h to include linked list for snake

--- TP-Jogo/src/snake.h
+++ TP-Jogo/src/snake.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include <time.h>
 
 #define LARGURA 660
@@ -20,11 +21,21 @@ typedef struct Bordas{
     Rectangle pos;
 }Bordas;
 
+typedef struct Body Body;
+
 typedef struct Body{
     Rectangle pos;
     Color color;
     int direction;
+    Body *next;
+    Body *prev;
 }Body;
+
+typedef struct Snake {
+    Body *head;
+    Body *tail;
+    int length;
+} Snake;
 
 typedef struct Food{
     Rectangle pos;
@@ -33,16 +44,19 @@ typedef struct Food{
 
 typedef struct Game{
     Body body;
+    Snake snake;
     Food food;
     Bordas bordas[4];
     double time;
     double cooldown;
 }Game;
 
+Body* CreatSegment (float x, float y, Color color, Rectangle pos);
 void StartBody(Game *g);
 void StartBordas(Game *g);
 void StartFood(Game *g);
 void StartRound(Game *g);
+void MoveGrowSnake(Game *g, int, int, bool);
 void DrawBody(Game *g);
 void DrawFood(Game *g);
 void DrawBordas(Game *g);
@@ -51,6 +65,7 @@ void DrawFinalScreen(Game *g);
 void Drawgame(Game *g);
 void Updatedirection(Game *g);
 void UpdatePosBody(Game *g);
+void UpdatePosFood(Game *g);
 void UpdateRodada(Game *g);
 int CollisionFood(Game *g);
 
